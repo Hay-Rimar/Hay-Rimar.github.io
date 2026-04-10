c73c8cbd7047b85f504db934a6421208ca8ff86d
Hay-Rimar <hayley.rimar@dcmail.ca>
2026-04-10 13:08:30 -0400

    Commit 3
    found missing css code, linked css and js to html, added data objects into js

--- project4/part2/script.js
+++ project4/part2/script.js
@@ -3,3 +3,13 @@ const thumbBar = document.querySelector(".thumb-bar");
 
 const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
+
+// Data objects
+
+const images = [
+    { filename: "pic1.jpg", alt: "Human eye closeup" },
+    { filename: "pic2.jpg", alt: "Rock with a wave in it" },
+    { filename: "pic3.jpg", alt: "Purple and white flowers" },
+    { filename: "pic4.jpg", alt: "Wall from pharaoh's tomb" },
+    { filename: "pic5.jpg", alt: "Moth on a leaf" },
+];
